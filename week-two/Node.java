542fd239c9f5828e4afe6eeb8cda9bdd80153763
Olivia Steger <ogsteger@gmail.com>
2019-05-17 20:15:28 -0400

    Create Node.java

---
+++ week-two/Node.java
@@ -0,0 +1,25 @@
+public class Node {
+  private String data;
+  public Node next;
+
+  public Node(String data) {
+    this.data = data;
+    this.next = null;
+  }
+
+  public String getData() {
+    return this.data;
+  }
+
+  public void setData(String data) {
+    this.data = data;
+  }
+
+  public Node getNext() {
+    return this.next;
+  }
+
+  public void setNext(Node next) {
+    this.next = next;
+  }
+}
